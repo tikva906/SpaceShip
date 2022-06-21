884e94f9ed08e0669aba22cc8c79994a6708f022
PLATON <platonfedoseev@gmail.com>
2022-06-21 18:40:42 +0300

    init

---
+++ ship.py
@@ -0,0 +1,16 @@
+import pygame
+class Ship:
+    def __init__(self, screen):
+        # объявить и инициализировать экран, который мы приняли
+        self.Screen = screen
+        # Получить сетку (координатную) экрана
+        self.ScreenRect = self.Screen.get_rect()
+        # Загрузить картинку корабля
+        self.image = pygame.image.load("Images/ship.png")
+        # Получить сетку этой картинки
+        self.rect = self.image.get_rect()
+        # Отпозиционировать сетку картинки на сетке экрана
+        self.rect.midbottom = self.ScreenRect.midbottom
+    def Blitme(self):
+        self.Screen.blit(self.image,self.rect)
+
